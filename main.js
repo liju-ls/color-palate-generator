073d1cf3cb6efc4c7f585393053d083e63c2c570
LIJU <liju.lukas@gmail.com>
2024-02-12 17:55:41 +0530

    navbar reworked and generate colors when startup

--- main.js
+++ main.js
@@ -4,18 +4,21 @@ let colors = document.getElementsByClassName('color');
 let colorsArray = [...colors];
 let generateBtn = document.getElementById('generateBtn');
 
+
 let generatePallete = () => {
     colorsArray.forEach((element) => {
         let colorValue = "#";
-
+        
         for (let i = 0; i < 6; i++)
-            colorValue += hexColorValues[Math.floor(Math.random() * hexColorValues.length)];
-
-        console.log(element.firstElementChild.innerHTML = `${colorValue}`);
-        element.setAttribute("style", `background-color:${colorValue};`);
-    });
+        colorValue += hexColorValues[Math.floor(Math.random() * hexColorValues.length)];
+    
+    console.log(element.firstElementChild.innerHTML = `${colorValue}`);
+    element.setAttribute("style", `background-color:${colorValue};`);
+});
 }
 
 generateBtn.addEventListener("click", () => {
     generatePallete();
 });
+
+generatePallete();
